cb6a4947aa9e4bb45efa03c3688ad09325c6955c
AnkitLipate <130392155+AnkitLipate@users.noreply.github.com>
2024-04-17 14:29:12 +0530

    Add files via upload

---
+++ day4_lab/src/main/java/dao/Topic.java
@@ -0,0 +1,32 @@
+package dao;
+
+public class Topic {
+	private int topicId;
+	private String topicName;
+	
+	public Topic() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getTopicId() {
+		return topicId;
+	}
+
+	public void setTopicId(int topicId) {
+		this.topicId = topicId;
+	}
+
+	public String getTopicName() {
+		return topicName;
+	}
+
+	public void setTopicName(String topicName) {
+		this.topicName = topicName;
+	}
+
+	@Override
+	public String toString() {
+		return "Topic [topicId=" + topicId + ", topicName=" + topicName + "]";
+	}
+	
+}
